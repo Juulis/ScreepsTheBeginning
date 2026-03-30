2050dc7fe34e175038fd2ed962ae14a463209648
Juulis <danny.p@live.se>
2026-03-30 21:23:38 +0200

    fixed bug in buildhandler

--- build.handler.js
+++ build.handler.js
@@ -21,7 +21,7 @@ var builder = {
                 }
             }
             if(Memory.debug)console.log(`cap: ${room.energyCapacityAvailable} crl: ${room.controller.level} ext: ${totalExtensions} extSites: ${totalExtensionConstructionsites}`);
-            if (room.energyCapacityAvailable > 500 && room.controller.level > 2 && totalExtensions < 6 && totalExtensionConstructionsites < 0) {
+            if (room.energyCapacityAvailable > 500 && room.controller.level > 2 && totalExtensions < 6 && totalExtensionConstructionsites < 1) {
                 console.log("building second extensions at ", spawnPos);
                 const posX = [spawnPos.x - 3, spawnPos.x + 3, spawnPos.x, spawnPos.x, spawnPos.x + 2];
                 const posY = [spawnPos.y, spawnPos.y, spawnPos.y - 3, spawnPos.y + 3, spawnPos.y + 3];
@@ -31,7 +31,7 @@ var builder = {
                     }
                 }
             }
-            if (room.controller.level > 3 && totalExtensions < 15 && totalExtensionConstructionsites < 0) {
+            if (room.controller.level > 3 && totalExtensions < 15 && totalExtensionConstructionsites < 1) {
                 console.log("building second extensions at ", spawnPos);
                 const posX = [spawnPos.x - 2, spawnPos.x + 4, spawnPos.x, spawnPos.x, spawnPos.x + 2];
                 const posY = [spawnPos.y - 2, spawnPos.y, spawnPos.y - 4, spawnPos.y + 4, spawnPos.y + 4];
