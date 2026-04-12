c3622b62e1bb77951fa9bf4ba0f0d651180870fa
Juulis <danny.p@live.se>
2026-04-12 08:38:45 +0200

    bugfix harvester mass spawn bug

--- creepHandler.js
+++ creepHandler.js
@@ -43,6 +43,7 @@ var creepHandler = {
         const roomsWithSources = Object.keys(Memory.rooms).filter(roomName =>
             Memory.rooms[roomName].sources && Object.keys(Memory.rooms[roomName].sources).length > 0
         );
+        const mainRoom = room.name === Memory.mainRoom;
         let max_harvesters = Object.keys(Memory.sources).length * 2;
         let max_builders = 1;
         let max_upgraders = 1;
@@ -358,7 +359,7 @@ var creepHandler = {
         } else if (buildersTotal < max_builders && harvestersTotal > 5 && constructionSitesExist) {
             if (Memory.debug) console.log(`creating builder - balance`);
             spawnBuilder();
-        } else if (harvestersTotal < max_harvesters) {
+        } else if (mainRoom && harvestersTotal < max_harvesters) { // only spawn harvesters in mainRoom, or stage < 4 rooms will spawn bunch of harvesters that runs to source[1] in mainRoom
             spawnHarvester();
             if (Memory.debug) console.log(`creating harvester`);
         } else if (haulersTotal < max_haulers && (containersTotal > 0 || storageExist)) {
